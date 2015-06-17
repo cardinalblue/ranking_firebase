83234d72c85ed911d785f59f61612a3245ecc1b2
PRADA Hsiung <bear.prada@gmail.com>
2015-06-17 17:45:29 +0800

    integrate add page with react route

--- js/app.js
+++ js/app.js
@@ -1,38 +1,59 @@
-import React  from 'react';  
-import Router from 'react-router';  
+import React  from 'react';
+import Router from 'react-router';
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
 import PopularPageHandler from './components/PopularPage.js';
 import VotePageHandler    from './components/VotePage.js';
+import AddPageHandler     from './components/AddPage.js';
+import auth from './auth';
 
-
-
-let App = React.createClass({  
-  render() {
-    return (
-      <div className="nav">
-        <Link to="app">Home</Link>
-        <Link to="popularPage">PopularPage</Link>
-        {/* this is the importTant part */}
-        <RouteHandler/>
-      </div>
-    );
-  }
+let App = React.createClass({
+    getInitialState() {
+        return {authData: null};
+    },
+    clickLogin (e) {
+      var self = this;
+      auth.login(function(e, authData) {
+        self.setState({authData: authData});
+      });
+    },
+    render() {
+        {/* FIXME navId is not work after integarte react route */}
+        return <div>
+                <nav className="navbar navbar-default navbar-static-top ">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">
+                                Ranking
+                            </a>
+                        </div>
+                        { !this.state.authData ? null : <ul className="nav navbar-nav navbar-right">
+                            <li><Link to="addPage" className="btn" role="button">Add List</Link></li>
+                        </ul> }
+                        { !!this.state.authData ? null : <ul className="nav navbar-nav navbar-right">
+                            <li><a className="btn" role="button" onClick={this.clickLogin}>Login</a></li>
+                        </ul> }
+                    </div>
+                </nav>
+                <RouteHandler/>
+               </div>;
+    }
 });
 
-let routes = (  
+let routes = (
   <Route name="app" path="/" handler={App}>
-    
+
     <Route name="popularPage" path="/popularPage" handler={PopularPageHandler}>
       {/*TODO: need to figure out why it cannot work*/}
       {/*<Route name="votePage" path=":itemName" handler={VotePageHandler} />*/}
     </Route>
-    <Route name="votePage" path="/popularPage/:itemName" handler={VotePageHandler} />  
-    
+    <Route name="votePage" path="/popularPage/:itemName" handler={VotePageHandler} />
+    <Route name="addPage" path="/addPage/" handler={AddPageHandler} />
+
     <DefaultRoute  handler={PopularPageHandler}/>
   </Route>
 );
 
-Router.run(routes, function (Handler) {  
+Router.run(routes, function (Handler) {
   React.render(<Handler/>, document.body);
 });
---
+++ js/components/AddPage.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import Router from 'react-router';
+import auth from '../auth';
+import { DefaultRoute, Link, Route, RouteHandler, Navigation } from 'react-router';
+
+let AdderList = React.createClass({
+    getInitialState() {
+      return {items: [], item_name: "", item_url: ""};
+    },
+    onNameChange(e) {
+        this.setState({item_name: e.target.value});
+    },
+    onUrlChange(e) {
+        this.setState({item_url: e.target.value});
+    },
+    addItem(e) {
+        var nextItems = this.state.items.concat([{
+            name : this.state.item_name,
+            url : this.state.item_url}]);
+        this.setState({items: nextItems, item_name: '', item_url: ''});
+        this.props.syncData(nextItems);
+    },
+    render() {
+        var createItem = function(item, index) {
+            return <tr>
+                    <td>{item.name}</td>
+                    <td>{item.url}</td>
+                    <td>Delete(TODO)</td>
+                 </tr>;
+        };
+        return <tbody>
+                {this.state.items.map(createItem)}
+                <tr>
+                    <td><input onChange={this.onNameChange} value={this.state.item_name} /></td>
+                    <td><input onChange={this.onUrlChange} value={this.state.item_url} /></td>
+                    <td><input type="button" onClick={ this.addItem } value="Add" /></td>
+                </tr>
+               </tbody>;
+    }
+});
+
+var AddPage = React.createClass({
+    mixins: [Navigation],
+    getInitialState() {
+        return {title: "", authData: auth.getAuthData() };
+    },
+    onTitleChange(e) {
+        this.setState({title: e.target.value});
+    },
+    onSavedList(e) {
+        if (!e) { this.transitionTo('votePage', {itemName: this.state.title}) }
+    },
+    onSavedUser(e) {
+        // TODO check the key is duplicate or not. if it's exist we will override this values
+        var ref = new Firebase("https://prada-test.firebaseio.com/items/" + this.state.title);
+        var len = this.items.length; // FIXME list get from VoteList
+        var list = {};
+        for (var i = 0; i < len; i++) {
+            var t = this.items[i];
+            list[t.name] = t;
+            list[t.name].vote = 0;
+        }
+        ref.set({
+            owner: this.state.authData.uid,
+            name: this.state.title,
+            list: list
+        }, this.onSavedList);
+    },
+    clickSubmit(e) {
+        if (!this.state.authData) {
+            this.clickBack(null);
+            return;
+        }
+        var uid = this.state.authData.uid;
+        var userRef = new Firebase("https://prada-test.firebaseio.com/Users/" + uid);
+        // save user data first
+        userRef.set(this.state.authData, this.onSavedUser);
+        this.onSavedUser(null);
+    },
+    syncData(items) {
+        this.items = items;
+    },
+    render() {
+      return <div>
+                <div className="jumbotron">
+                    <h1>Add</h1>
+                </div>
+                <div className="panel panel-default">
+                  <div className="panel-heading">
+                      <input onChange={this.onTitleChange} value={this.state.title} placeholder="Title" />
+                  </div>
+                  <table className="table">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Url</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <AdderList syncData={this.syncData}/>
+                  </table>
+                </div>
+                <input type="button" onClick={ this.clickSubmit } value="Submit" />
+             </div>;
+    }
+});
+
+
+export default AddPage;
